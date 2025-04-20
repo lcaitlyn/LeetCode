9d3c6edb50d2b574b431dc55eeaf1371b1adfdc5
lcaitlyn <mrrifatcool@gmail.com>
2025-04-20 22:29:16 +0300

    обновил binarySearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,67 +1,87 @@
 public class BinarySearch {
-    public static int search(int[] nums, int target) {
-        int min = 0;
-        int max = nums.length - 1;
-        int med = max / 2;
-        while (true) {
-            System.out.println("med = " + med);
-            if (med > nums.length - 1) {
-                return -1;
-            }
+    // public static int search(int[] nums, int target) {
+    //     int min = 0;
+    //     int max = nums.length - 1;
+    //     int med = max / 2;
+    //     while (true) {
+    //         System.out.println("med = " + med);
+    //         if (med > nums.length - 1) {
+    //             return -1;
+    //         }
 
-            if (max - min < 3) {
-                if (min > nums.length - 1) {
-                    return -1;
-                }
+    //         if (max - min < 3) {
+    //             if (min > nums.length - 1) {
+    //                 return -1;
+    //             }
 
-                if (nums[med] == target) {
-                    return med;
-                } else if (nums[min] == target) {
-                    return min;
-                } else if (nums[max] == target) {
-                    return max;
-                } else {
-                    return -1;
-                }
-            }
+    //             if (nums[med] == target) {
+    //                 return med;
+    //             } else if (nums[min] == target) {
+    //                 return min;
+    //             } else if (nums[max] == target) {
+    //                 return max;
+    //             } else {
+    //                 return -1;
+    //             }
+    //         }
 
-            if (med == 0 && nums[med] != target) {
-                return -1;
-            }
+    //         if (med == 0 && nums[med] != target) {
+    //             return -1;
+    //         }
             
+    //         if (nums[med] == target) {
+    //             return med;
+    //         }
+
+    //         if (nums[med] > target) {
+    //             max = med - 1;
+    //             med = max / 2;
+    //         }
+
+    //         if (nums[med] < target) {
+    //             min = med + 1;
+    //             med = (max - min) / 2 + min;
+    //         }
+
+    //         System.out.println("min = " + min + ", med = " + med + ", max = " + max);
+
+    //     }
+    // }
+
+    // public static int searchRec(int [] nums, int target, int lo, int hi) {
+    //     if (lo > hi) {
+    //         return -1;
+    //     }
+    //     int mid = (hi - lo) / 2 + lo;
+    //     System.out.println("min = " + lo + ", med = " + mid + ", max = " + hi);
+    //     if (nums[mid] == target) {
+    //         return mid;
+    //     }
+    //     if (nums[mid] < target) {
+    //         return searchRec(nums, target, mid + 1, hi);
+    //     }
+    //     return searchRec(nums, target, lo, hi - 1);
+    // }
+
+
+    public int search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int med = (hi - lo) / 2 + lo;
             if (nums[med] == target) {
                 return med;
+            } else if (target < nums[med]) {
+                hi = med - 1;
+            } else {
+                lo = med + 1;
             }
-
-            if (nums[med] > target) {
-                max = med - 1;
-                med = max / 2;
-            }
-
-            if (nums[med] < target) {
-                min = med + 1;
-                med = (max - min) / 2 + min;
-            }
-
-            System.out.println("min = " + min + ", med = " + med + ", max = " + max);
-
         }
+        return -1;
     }
 
-    public static int searchRec(int [] nums, int target, int lo, int hi) {
-        if (lo > hi) {
-            return -1;
-        }
-        int mid = (hi - lo) / 2 + lo;
-        System.out.println("min = " + lo + ", med = " + mid + ", max = " + hi);
-        if (nums[mid] == target) {
-            return mid;
-        }
-        if (nums[mid] < target) {
-            return searchRec(nums, target, mid + 1, hi);
-        }
-        return searchRec(nums, target, lo, hi - 1);
-    }
+    // мой стандартный шаблон binary search (добавил 20 апреля 2025)
 
     public static void main(String[] args) {
         int [] arr = new int[] {-1,0,3,5,9,12};
